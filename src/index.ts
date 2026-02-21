ae0c7402076ba9b6f6bc50b05e9bf59d17988d80
nick-p-34 <117747730+nick-p-34@users.noreply.github.com>
2026-02-21 18:18:47 +0000

    Remove index.ts, point Wrangler to worker

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    async fetch(request: Request) {
-        return new Response('Hello from Zenith Racing Team!', {
-            headers: { 'Content-Type': 'text/plain' },
-        });
-    },
-};
